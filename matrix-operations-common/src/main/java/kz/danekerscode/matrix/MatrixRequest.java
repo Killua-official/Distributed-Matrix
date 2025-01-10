a4127c8060137446584ed5db04ddfac0a6559ff1
Aruzhan Zharkenova <120177140+Killua-official@users.noreply.github.com>
2025-01-10 13:14:41 +0500

    Update MatrixRequest.java
    feat: add matrix addition method with dimension validation

--- matrix-operations-common/src/main/java/kz/danekerscode/matrix/MatrixRequest.java
+++ matrix-operations-common/src/main/java/kz/danekerscode/matrix/MatrixRequest.java
@@ -19,4 +19,22 @@ public class MatrixRequest {
     public void setMatrixB(double[][] matrixB) {
         this.matrixB = matrixB;
     }
+
+    public double[][] addMatrices() {
+        if (!canAdd()) {
+            throw new IllegalArgumentException("Matrices dimensions do not match for addition");
+        }
+        double[][] result = new double[matrixA.length][matrixA[0].length];
+        for (int i = 0; i < matrixA.length; i++) {
+            for (int j = 0; j < matrixA[0].length; j++) {
+                result[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+        return result;
+    }
+
+    private boolean canAdd() {
+        return matrixA.length == matrixB.length && matrixA[0].length == matrixB[0].length;
+    }
 }
+
